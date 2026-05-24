813f819619003287fcc3b9d3556e7ce9ec5fca1a
makisan <48534295+sandonemaki@users.noreply.github.com>
2026-05-24 15:52:36 +0900

    lambdaのgoコード

---
+++ sam-tf-app/functions/hello/main.go
@@ -0,0 +1,20 @@
+package hello
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Response struct {
+	StatusCode int    `json:"statusCode"`
+	Body       string `json:"body"`
+}
+
+func handler(ctx context.Context) (Response, error) {
+	return Response{StatusCode: 200, Body: "Hello!"}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
